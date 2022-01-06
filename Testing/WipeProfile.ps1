6d1dbdfea5802bc33cabf90645990007dab45d14
nicholashaynes1 <nicholashaynes1@gmail.com>
2022-01-06 11:50:19 -0700

    Updated wiped accounts
    I updated the accounts the script picks up to avoid deleting any necessary users.

--- Testing/WipeProfile.ps1
+++ Testing/WipeProfile.ps1
@@ -1,2 +1,2 @@
-﻿$keepUser = "bus-itgroup"
+﻿$keepUser = "bus-itgroup", "Administrator", "WDAGUtilityAccount", "DefaultAccount", "u0512666", "Guest"
 Get-LocalUser | ?{$keepUser -notcontains $_.Name} | Remove-LocalUser -WhatIf
